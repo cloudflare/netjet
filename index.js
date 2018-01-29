9d195a49734455c3fc84198311eb1812000c2c58
Samy Pessé <samypesse@gmail.com>
2018-01-29 21:50:39 +0100

    Add option "directives"

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ module.exports = function netjet(options) {
     scripts: true,
     styles: true,
     cache: {},
+    directives: [],
   });
 
   var cache = new LRU(options.cache);
+  var directives = [''].concat(options.directives).join('; ');
 
   return function netjetMiddleware(req, res, next) {
     function appendHeader(field, value) {
@@ -60,7 +62,7 @@ module.exports = function netjet(options) {
               (addBaseHref ? baseTag[0] : '') +
               encodeRFC5987(unescape(url)) +
               '>; rel=preload; as=' +
-              asType
+              asType + directives
           );
         });
     }
